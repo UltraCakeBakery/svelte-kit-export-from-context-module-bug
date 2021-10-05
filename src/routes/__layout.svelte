0757c299b15a96adec3c9fb37fce4e4dbb5699a9
Jack van der Bilt <jack@managing.software>
2021-10-05 12:10:39 +0200

    feat: example

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,27 @@
+<script context="module">
+    import { writable } from "svelte/store";
+    export const now = new writable(new Date())
+</script>
+
+<script>
+    import { onDestroy, onMount } from "svelte";
+
+    let interval;
+    onMount( () =>
+    {
+        interval = setInterval( () => {
+            $now = new Date()
+        })
+    })
+
+    onDestroy( () => clearInterval( interval ) )
+</script>
+
+
+<strong>Is updating</strong>:
+<br/>
+{ $now }
+<br/>
+<br/>
+
+<slot></slot>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { now } from './__layout.svelte'
+</script>
+
+
+<strong>Should be updating</strong>:
+<br/>
+{ $now }
